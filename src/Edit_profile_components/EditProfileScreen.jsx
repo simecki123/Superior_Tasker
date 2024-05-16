464335a89f513c36153f3f85ef5e11df426ee031
Sime Roncevic <sroncevic19@gmail.com>
2024-05-16 18:31:55 +0200

    Updating edit profile css

--- src/Edit_profile_components/EditProfileScreen.jsx
+++ src/Edit_profile_components/EditProfileScreen.jsx
@@ -3,26 +3,26 @@ import './EditProfile-dark.css'; // Import the CSS file for styling
 
 function EditProfileScreen() {
     return (
-        <div className="edit-profile-screen">
-            <div className="picture-dropzone">
-                <input type="file" accept="image/*" />
+        <div className="edit-profile-screen-container">
+            <div className="picture-dropzone-container">
+                <input className='picture-dropzone-input' type="file" accept="image/*" />
                 <p>Drag & Drop or Click to Upload Picture</p>
             </div>
 
-            <div className="user-info">
-                <h2 className='title'>Name:</h2>
-                <input className="user-input" placeholder="Enter your name..." />
+            <div className="edit-profile-user-info-container">
+                <h2 className='edit-profile-name-title'>Name:</h2>
+                <input className="edit-profile-enter-name-user-input" placeholder="Enter your name..." />
 
-                <h2 className='title'>Surname:</h2>
-                <input className="user-input" placeholder="Enter your surname..." />
+                <h2 className='edit-profile-name-title'>Surname:</h2>
+                <input className="edit-profile-enter-name-user-input" placeholder="Enter your surname..." />
 
-                <h2 className='title'>Description:</h2>
-                <textarea className='description' placeholder='Enter description' ></textarea>
+                <h2 className='edit-profile-name-title'>Description:</h2>
+                <textarea className='description-edit-profile' placeholder='Enter description' ></textarea>
 
                 
             </div>
             <Link to={"/mainpage"}>
-                <button className="save-btn">SAVE</button>
+                <button className="save-btn-edit-profile">SAVE</button>
             </Link>
         </div>
     );
