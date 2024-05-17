0b3ba7be2153e81126ba231d7fc8d0dc09edc0c7
Sime Roncevic <sroncevic19@gmail.com>
2024-05-17 20:28:36 +0200

    Fixing profilecomponent css.

--- src/main_page_components/Profile_components/Profile-component.jsx
+++ src/main_page_components/Profile_components/Profile-component.jsx
@@ -5,12 +5,12 @@ import PropTypes from 'prop-types';
 function ProfileScreen({ user }) {
   return (
     <div className="Profile-component">
-      <img src={user.img} alt='LOGO' className='big-logo' />
-      <h1 className='big-Name-surname'>{user.firstName + " "+ user.lastName}</h1>
-      <h3 className='profile-description'>Description</h3>
-      <p>{user.description}</p>
+      <img src={user.img} alt='LOGO' className='profile-component-big-logo' />
+      <h1 className='profile-component-big-name-surname'>{user.firstName + " "+ user.lastName}</h1>
+      <h3 className='profile-component-profile-description'>Description</h3>
+      <p className='profile-component-description-text'>{user.description}</p>
       <Link to={"/editprofile"}>
-        <button>EDIT PROFILE</button>
+        <button className='profile-component-edit-button'>EDIT PROFILE</button>
       </Link>
     </div>
   );
